8043cbd4e6aee7248f531f0b4255dd9b4e556e6c
modolee <lsi1318@gmail.com>
2018-07-25 15:47:29 +0900

    Flask API 구현

--- node_server.py
+++ node_server.py
@@ -2,6 +2,9 @@ from hashlib import sha256
 import json
 import time
 
+from flask import Flask, request
+import requests
+
 class Block:
     def __init__(self, index, transactions, timestamp, previous_hash):
         self.index = index  # 블록 번호
@@ -118,3 +121,43 @@ class Blockchain:
     @property
     def last_block(self):
         return self.chain[-1]
+
+
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/new_transaction', methods=['POST'])
+def new_transaction():
+    tx_data = request.get_json()
+    required_fields = ["author", "content"]
+
+    for field in required_fields:
+        if not tx_data.get(field):
+            return "Invalid transaction data", 404
+
+    tx_data["timestamp"] = time.time()
+    blockchain.add_new_transaction(tx_data)
+
+    return "Success", 201
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dump({"length": len(chain_data),
+                      "chain": chain_data})
+
+@app.route('/mine', methods=['GET'])
+def mine_unconfirmed_transactions():
+    result = blockchain.mine()
+    if not result:
+        return "No transactions to mine"
+    return "Block #{} is mined.".format(result)
+
+@app.route('/pending_tx')
+def get_pending_tx():
+    return json.dumps(blockchain.unconfirmed_transactions)
+
+app.run(debug=True, port=7000)
