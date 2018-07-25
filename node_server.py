6ba3d91fcc2f6d3448252d73ad535f90dfc02a3d
modolee <lsi1318@gmail.com>
2018-07-25 16:35:00 +0900

    오타 수정

--- node_server.py
+++ node_server.py
@@ -146,7 +146,7 @@ def get_chain():
     chain_data = []
     for block in blockchain.chain:
         chain_data.append(block.__dict__)
-    return json.dump({"length": len(chain_data),
+    return json.dumps({"length": len(chain_data),
                       "chain": chain_data})
 
 @app.route('/mine', methods=['GET'])
